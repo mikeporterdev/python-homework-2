418f1842c5a7fceec34ff101cc9ec5f8aecf0ecd
Michael Porter <m_porter@live.co.uk>
2017-03-16 17:49:17 +0000

    Initial Commit

---
+++ Homework2.py
@@ -0,0 +1,71 @@
+'''
+Welcome to Homework #2.
+
+This one has a few more methods for you to implement.
+When you start implementing a method, remember to delete the line that says "pass", that just exists to stop python
+exploding because there's no code within a method!.
+
+When you think you've got a method done, run the automatic tests in test_homework.py. I've tried to include helpful
+messages if your method doesn't quite pass a test that might guide you along the way, so pay attention to the text in
+the console if the test isn't passing. If you're still struggling after that, ask each other or me for help and I'll do
+my best.
+
+If you haven't implemented all of the methods, the tests for the other methods will appear as failed, so don't worry if
+you see a lot of red symbols when you run it for the first time, just pay attention to the test for the method you're
+currently implementing.
+
+You can delete the comments if they're cluttering the screen for you. Just remember what they say if you need help ;)
+
+I will ask you to submit your homework back up to GitHub once you've done it so I can see the results this time.
+I will check everyones answers on the 30th or when everyone says they're done, whichever comes first.
+'''
+
+
+# Write a function here that will add up all the numbers into a list and return the total.
+# ie. [4, 7, 12, 16] returns 39
+def sum_all_in_list(list):
+    pass
+
+
+# Write a function here that will loop through a list and return all the strings in it added together and reversed
+# ie. Passing in ["Hi", "There", "My", "Name", "Is", "Mike"] returns "ekiMsIemaNyMerehTiH" all as one string
+# (Might need to look up the function on how to reverse a string for this one)
+def reverse_list_of_text(list):
+    pass
+
+
+# Write a function that will merge a list of lists together. ie. [[4, 2, 6], [2, 4, 7], [9, 2, 6]]
+# becomes one list of [4, 2, 6, 2, 4, 7, 9, 2, 6]
+def merge_two_lists(list):
+    pass
+
+
+# Write a function that will find the smallest number in a list and return it.
+# ie. [6, 2, 7, 99] returns 2. (There are two main ways you can do this, bonus points if you can find both)
+def get_smallest_number_in_list(list):
+    pass
+
+
+# Write a function that will remove all even numbers from a list of numbers.
+# You can do this by creating a new list, looping through the old list and adding the odd numbers to the new list
+# Don't try and remove numbers from a list you're currently looping through
+# ie. [2, 5, 8, 2, 3, 7] becomes [5, 3, 7]
+# If there are no odd numbers in the whole list, return an empty list
+def remove_even_numbers(list):
+    pass
+
+
+# append a string to all elements in a list and return the list of all.
+# ie. append_to_all_elements(["hey", "there", "friend"], "lol") returns ["heylol", "therelol", "friendlol"]
+def append_to_all_elements(list, string):
+    pass
+
+
+# return a list of each character in a string
+# ie. list_of_characters_in_string("Hello") returns ["H", "e", "l", "l", "o"]
+# Hint, doing 'for x in "string":' will loop through each character in the string.
+def list_of_characters_in_string(string):
+    pass
+
+
+# Use the space below this line to call your methods and print out the results to check you've got the answer.
---
+++ test_homework.py
@@ -0,0 +1,58 @@
+from unittest import TestCase
+
+import Homework2
+
+
+class TestSumAllInList(TestCase):
+    def test_sum_all_in_list(self):
+        sum = Homework2.sum_all_in_list([4, 6, 2, 8, 5])
+        self.assertEqual(sum, 25, "The addition here is not working")
+
+        sum = Homework2.sum_all_in_list([-1, 0, -4])
+        self.assertEqual(sum, -5, "Your sum method can't handle negative numbers")
+
+    def test_reverse_list_of_Text(self):
+        reverse = Homework2.reverse_list_of_text(["hi", "there"])
+        self.assertEqual(reverse, "erehtih", "Your reverse method isn't returning what I expected")
+
+        reverse = Homework2.reverse_list_of_text(["wOwZaS!"])
+        self.assertEqual(reverse, "!SaZwOw", "Your reverse method can't seem to handle capitalization")
+
+    def test_merge_two_lists(self):
+        merge = Homework2.merge_two_lists([[4, 2, 6], [2, 6, 8], [21, 2, 4]])
+        self.assertEqual(merge, [4, 2, 6, 2, 6, 8, 21, 2, 4], "Your merge method isn't returning what I expected")
+
+    def test_smallest_in_list(self):
+        smallest = Homework2.get_smallest_number_in_list([2, 5, 7])
+        self.assertEqual(smallest, 2, "Your smallest number method isn't returning what I expected")
+
+        smallest = Homework2.get_smallest_number_in_list([5, 2, 7])
+        self.assertEqual(smallest, 2, "Your smallest number method isn't returning what I expected")
+
+        smallest = Homework2.get_smallest_number_in_list([2, 2, 6])
+        self.assertEqual(smallest, 2, "Your smallest number method isn't returning what I expected")
+
+        smallest = Homework2.get_smallest_number_in_list([2, -2, -7])
+        self.assertEqual(smallest, -7, "Your smallest number method acts weirdly when the list has negatives in")
+
+    def test_remove_even_numbers(self):
+        removed = Homework2.remove_even_numbers([2, 3, 4])
+        self.assertEqual(removed, [3], "Your remove even numbers method isn't returning what I expected")
+
+        removed = Homework2.remove_even_numbers([4, 2, 6])
+        self.assertEqual(removed, [], "Your remove even numbers method doesn't work when only even numbers are passed in")
+
+        removed = Homework2.remove_even_numbers([1, 3, 5])
+        self.assertEqual(removed, [], "Your remove even numbers method doesn't work when only odd numbers are passed in")
+
+    def test_append_to_all_elements(self):
+        appended = Homework2.append_to_all_elements(["hey", "there", "friend"], "test")
+        self.assertEqual(appended, ["heytest", "theretest", "friendtest"], "Your append to all elements isn't returning what I expected")
+
+    def test_list_of_chars(self):
+        chars = Homework2.list_of_characters_in_string("Hello")
+        self.assertEqual(chars, ["H", "e", "l", "l", "o"], "Your list of characters in string method isn't working like I expected")
+
+
+
+
